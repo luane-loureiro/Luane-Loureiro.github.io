daeed3cad01c3d59571b35978fc73ca79ccf7c67
luane-loureiro <luachan@gmail.com>
2024-10-23 22:22:08 -0300

    update data base conection

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 fetchProfileData()
 async function fetchProfileData() {
-    const response = await fetch('https://raw.githubusercontent.com/luane-loureiro/challenge-one-portfolio-br/master/data/profile.json')
+    const response = await fetch('https://raw.githubusercontent.com/luane-loureiro/Luane-Loureiro.github.io/refs/heads/main/data/profile.json')
     let profileData = await response.json()
     return(profileData)
 }
